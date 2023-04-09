4a2061858c83c09b7571ad0bb588c625eb4d1aa2
jaspalsinghchadha <jaspalchadha2@gmail.com>
2023-04-08 17:59:29 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide (2000,100);
+const square = (parameter1) => parameter1 * parameter1
+square (3,3);
+const add = (parameter1,parameter2) => parameter1 + parameter2
+add (2,2);
+
